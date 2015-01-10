f22cc543ef8d512321c2fa0d0569788b3556dcff
Henry Lovett <seblovett@googlemail.com>
2015-01-10 17:28:25 +0000

    LEDs are placed!
    Worked out how to script KiCAD to do it too. Winner winner, time for a chicken dinner

--- scripts/placeleds.py
+++ scripts/placeleds.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python
-# @file placeleds.py
-# Date Created: Fri 02 Jan 2015 15:24:17 GMT by seblovett on seblovett-Ubuntu
-# <+Last Edited: Fri 02 Jan 2015 15:40:55 GMT by seblovett on seblovett-Ubuntu +>
+# @file leds.py
+# Date Created: Sat 10 Jan 2015 16:32:28 GMT by seblovett on seblovett-Ubuntu
+# <+Last Edited: Sat 10 Jan 2015 17:25:45 GMT by seblovett on seblovett-Ubuntu +>
 # @author seblovett
-# @brief A brief description of this code
+# @brief A brief description of this codo
+from pcbnew import *
 import math
+#all measurements are in nanometers.... 
+ORIGIN = [105500000, 85200000] 
+DIST = -47000000
 
-DIST = 37.0 #distance from centre of circle
-ORIGIN = [40,40] # oriign of the circle
-LEDS = range(1,61)
-CMD = "MOVE LED%d (%f %f); "
-if "__main__" == __name__:
-	''' Code to be run if this is main '''
-	print LEDS
-	angles = list()
-	for LED in LEDS:
-		angles.append(2*math.pi*(LED / 60.0))
-	for i in range(len(angles)):
-		print CMD % (LEDS[i], math.sin(angles[i])*DIST+ORIGIN[0], math.cos(angles[i])*DIST+ORIGIN[1])
-		
-	#print angles
-	pass
-
+def LEDtoLoc(led):
+	
+	#calculate where the LED should be. 
+	x = math.sin(2*math.pi*led/60.0)*DIST + ORIGIN[0]
+	y = math.cos(2*math.pi*led/60.0)*DIST + ORIGIN[1]
+	pt = wxPoint(x,y)
+	return pt
+def LEDtoAngle(led):
+	a = 6*n*10+900#= math.sin(2*math.pi*led/60.0)
+	#print a
+	return a
+#def PlaceLeds():
+print "Hello, world!"
+pcb = GetBoard()
+mod = pcb.GetModules()
+while mod:
+	if(mod.GetReference().startswith("LED")):
+		#print mod.GetReference()
+		n = float(mod.GetReference()[3:])
+		mod.SetPosition(LEDtoLoc(n))
+		mod.SetOrientation(LEDtoAngle(n))
+	mod = mod.Next()
+#while(mod.GetReference != "LED1"):
+#	mod = mod.Next()
+#print mod.GetReference()
